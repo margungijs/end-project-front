886324062b26edf3d15744d683064a989fd70062
margungijs <131168640+margungijs@users.noreply.github.com>
2025-03-11 11:18:32 +0200

    cors...

--- src/reuse/SendData.js
+++ src/reuse/SendData.js
@@ -4,9 +4,11 @@ import { API_URL } from "../config";
 const SendData = async (data, api, loading) => {
     loading()
 
-
     await axios.get(
         `${API_URL}/sanctum/csrf-cookie`,
+        {
+            withCredentials: true
+        }
     );
 
     await axios.post(
