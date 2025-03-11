bd173d9e04cb094d156c11b1a4fa48c305c7ebae
margungijs <131168640+margungijs@users.noreply.github.com>
2025-03-11 13:37:21 +0200

    cors4

--- src/reuse/SendData.js
+++ src/reuse/SendData.js
@@ -2,33 +2,37 @@ import axios from "axios";
 import { API_URL } from "../config";
 
 const SendData = async (data, api, loading) => {
-    loading()
+    loading();
 
-    await axios.get(
-        `${API_URL}/sanctum/csrf-cookie`,
-        {
-            withCredentials: true
-        }
-    );
+
+    await axios.get(`${API_URL}/sanctum/csrf-cookie`, {
+        withCredentials: true,
+    });
+
+    const csrfToken = getCookie('XSRF-TOKEN');
+
+    if (!csrfToken) {
+        console.error('CSRF token not found');
+        return;
+    }
 
     await axios.post(
         api,
-        {...data},
+        { ...data },
         {
-            headers:{
+            headers: {
                 accept: 'application/json',
-                "X-XSRF-TOKEN": getCookie("XSRF-TOKEN"),
+                "X-XSRF-TOKEN": csrfToken,
             },
             withCredentials: true,
-            withXSRFToken: true
         }
-    )
-
-    function getCookie(name) {
-        const value = `; ${document.cookie}`;
-        const parts = value.split(`; ${name}=`);
-        if (parts.length === 2) return parts.pop().split(';').shift();
-    }
+    );
 };
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
 
 export default SendData;
